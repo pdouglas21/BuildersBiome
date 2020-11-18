0870e69cc2b2ce4bfa1bb9cabcbbf43f17d74b6e
pdouglas <pdouglas21@holynames-sea.org>
2020-11-18 07:20:52 -0800

    Created subpages and added header picture files (they don't show up on the website)

--- src/main/java/service/MainController.java
+++ src/main/java/service/MainController.java
@@ -3,15 +3,20 @@ package service;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
 
-	@GetMapping("/home")
-	public String home(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-		model.addAttribute("name", name);
+	@GetMapping("/")
+	public String home() {
 		return "home";
 	}
 
+	@GetMapping("/info/{category}")
+	public String category(@PathVariable("category")String category, Model model) {
+		model.addAttribute("category", category);
+		return category;
+	}
 }
